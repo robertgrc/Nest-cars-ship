f8e99fe5dfde238b9167c11f3cf7b13482c9e93f
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-01-16 17:52:22 -0400

    Interfaces y UUID

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -15,7 +15,7 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById(@Param('id', ParseIntPipe) id: number){
+    getCarById(@Param('id') id: string){
         console.log({id})       
         return this.CarsService.findOneById( id )   
     }
